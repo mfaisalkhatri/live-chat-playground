a6139827fa8948d22e6ca7a3866f0fdb63df3ef1
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2025-06-28 02:07:28 +0300

    updated chrome arguments in wdio config files

--- config/wdio.chrome.conf.ts
+++ config/wdio.chrome.conf.ts
@@ -11,7 +11,7 @@ config.capabilities = {
     capabilities: {
       browserName: "chrome",
       "goog:chromeOptions": {
-        args: ['headless', 'disable-gpu', '--start-maximized'],
+        args: ['--headless','--disable-gpu', '--start-maximized'],
       },
     },
   },
--- config/wdio.multiremote.conf.ts
+++ config/wdio.multiremote.conf.ts
@@ -8,7 +8,7 @@ config.capabilities = {
     capabilities: {
       browserName: "chrome",
       "goog:chromeOptions": {
-        args: ['headless', 'disable-gpu', '--start-maximized'],
+        args: ['--headless', '--disable-gpu', '--start-maximized'],
       },
     },
   },
@@ -16,7 +16,7 @@ config.capabilities = {
     capabilities: {
       browserName: "firefox",
       "moz:firefoxOptions": {
-        args: ['-headless',"--width=1280", "--height=720"],
+        args: ['--headless',"--width=1280", "--height=720"],
       },
     },
   },
